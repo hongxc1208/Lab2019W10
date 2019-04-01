3028af4b4f45a9fabcccea54defd80204ab9dc63
HongxiChen <chenh640@newschool.edu>
2019-04-01 16:56:19 -0400

    subway

---
+++ myjavascript.js
@@ -0,0 +1,49 @@
+    var container = document.querySelector(".container");
+
+
+    container.addEventListener("touchstart", startTouch, false);
+    container.addEventListener("touchmove", moveTouch, false);
+
+    // Swipe Up / Down / Left / Right
+    var initialX = null;
+    var initialY = null;
+
+    function startTouch(e) {
+      initialX = e.touches[0].clientX;
+      initialY = e.touches[0].clientY;
+    };
+
+    function moveTouch(e) {
+      if (initialX === null) {
+        return;
+      }
+
+      if (initialY === null) {
+        return;
+      }
+
+      var currentX = e.touches[0].clientX;
+      var currentY = e.touches[0].clientY;
+
+      var diffX = initialX - currentX;
+      var diffY = initialY - currentY;
+
+      if (Math.abs(diffX) > Math.abs(diffY)) {
+        // sliding horizontally
+        if (diffX > 0) {
+          // swiped left
+         
+          console.log("swiped left");
+          window.location.href = "06Experience2.html"
+        } else {
+          // swiped right
+          
+          console.log("swiped right");
+        }  
+      } 
+
+      initialX = null;
+      initialY = null;
+      
+      e.preventDefault();
+    };
